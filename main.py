c642f041d0e4c8b10a99aba1869137055b9c287c
Natan José de Almeida <natanalmeida158@hotmail.com>
2022-05-29 13:00:54 -0300

    Create a bullet

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import arcade
 import math
 import random
+import os
 
 SPRITE_SCALING = 0.5
+SPRITE_SCALING_LASER = 0.8
+SPRITE_SCALING_PLAYER = 0.5
+COIN_COUNT = 50
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -10,7 +14,29 @@ SCREEN_TITLE = "Move Sprite by Angle Example"
 
 MOVEMENT_SPEED = 5
 ANGLE_SPEED = 5
+BULLET_SPEED = 5
 
+EXPLOSION_TEXTURE_COUNT = 60
+
+class Explosion(arcade.Sprite):
+    """ This class creates an explosion animation """
+
+    def __init__(self, texture_list):
+        super().__init__()
+
+        # Start at the first frame
+        self.current_texture = 0
+        self.textures = texture_list
+
+    def update(self):
+
+        # Update to the next frame of the animation. If we are at the end
+        # of our frames, then delete this sprite.
+        self.current_texture += 1
+        if self.current_texture < len(self.textures):
+            self.set_texture(self.current_texture)
+        else:
+            self.remove_from_sprite_lists()
 
 class Player(arcade.Sprite):
     """ Player class """
@@ -48,14 +74,34 @@ class MyGame(arcade.Window):
 
         # Call the parent class initializer
         super().__init__(width, height, title)
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
 
 
         # Variables that will hold sprite lists
         self.player_list = None
         self.coin_list = None
+        self.bullet_list = None
+        self.explosions_list = None
 
         # Set up the player info
         self.player_sprite = None
+        self.score = 0
+        self.set_mouse_visible(False)
+        self.explosion_texture_list = []
+
+        columns = 16
+        count = 60
+        sprite_width = 256
+        sprite_height = 256
+        file_name = ":resources:images/spritesheets/explosion.png"
+
+        # Load the explosions from a sprite sheet
+        self.explosion_texture_list = arcade.load_spritesheet(file_name, sprite_width, sprite_height, columns, count)
+
+        # Load sounds. Sounds from kenney.nl
+        self.gun_sound = arcade.sound.load_sound(":resources:sounds/laser2.wav")
+        self.hit_sound = arcade.sound.load_sound(":resources:sounds/explosion2.wav")
 
         # Set the background color
         arcade.set_background_color(arcade.color.AMAZON)
@@ -67,6 +113,17 @@ class MyGame(arcade.Window):
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
         self.coin_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
+        self.explosions_list = arcade.SpriteList()
+
+        self.score = 0
+
+        # Image from kenney.nl
+        self.player_sprite = arcade.Sprite(":resources:images/animated_characters/female_person/femalePerson_idle.png",
+                                           SPRITE_SCALING_PLAYER)
+        self.player_sprite.center_x = 50
+        self.player_sprite.center_y = 70
+        self.player_list.append(self.player_sprite)
 
         # -- Set up the walls
 
@@ -140,9 +197,75 @@ class MyGame(arcade.Window):
         self.player_list.draw()
         self.wall_list.draw()
         self.coin_list.draw()
+        self.explosions_list.draw()
+        self.coin_list.draw()
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        """
+        Called whenever the mouse button is clicked.
+        """
+
+        # Gunshot sound
+        arcade.sound.play_sound(self.gun_sound)
+
+        # Create a bullet
+        bullet = arcade.Sprite(":resources:images/space_shooter/laserBlue01.png", SPRITE_SCALING_LASER)
+
+        # The image points to the right, and we want it to point up. So
+        # rotate it.
+        bullet.angle = 90
+
+        # Give it a speed
+        bullet.change_y = BULLET_SPEED
+
+        # Position the bullet
+        bullet.center_x = self.player_sprite.center_x
+        bullet.bottom = self.player_sprite.top
+
+        # Add the bullet to the appropriate lists
+        self.bullet_list.append(bullet)
 
     def on_update(self, delta_time):
         """ Movement and game logic """
+
+        self.bullet_list.update()
+        self.explosions_list.update()
+
+        # Loop through each bullet
+        for bullet in self.bullet_list:
+
+            # Check this bullet to see if it hit a coin
+            hit_list = arcade.check_for_collision_with_list(bullet, self.coin_list)
+
+            # If it did...
+            if len(hit_list) > 0:
+                # Make an explosion
+                explosion = Explosion(self.explosion_texture_list)
+
+                # Move it to the location of the coin
+                explosion.center_x = hit_list[0].center_x
+                explosion.center_y = hit_list[0].center_y
+
+                # Call update() because it sets which image we start on
+                explosion.update()
+
+                # Add to a list of sprites that are explosions
+                self.explosions_list.append(explosion)
+
+                # Get rid of the bullet
+                bullet.remove_from_sprite_lists()
+
+            # For every coin we hit, add to the score and remove the coin
+            for coin in hit_list:
+                coin.remove_from_sprite_lists()
+                self.score += 1
+
+                # Hit Sound
+                arcade.sound.play_sound(self.hit_sound)
+
+            # If the bullet flies off-screen, remove it.
+            if bullet.bottom > SCREEN_HEIGHT:
+                bullet.remove_from_sprite_lists()
 
         # Call update on all sprites (The sprites don't do much in this
         # example though.)
